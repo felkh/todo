4cc7e7aa7d5a4452fd88c9c2b22aea43cd11c1a4
felkh <felisa.kh@hotmail.com>
2019-07-31 18:16:55 +0000

    Ajax request on form submission

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
+  # skip_before_action :verify_authenticity_token
   def index
     render json: Task.all
   end
 
   def update
+    puts "hit the update spot"
     task = Task.find(params[:id])
     task.update_attributes(task_params)
     render json: task
